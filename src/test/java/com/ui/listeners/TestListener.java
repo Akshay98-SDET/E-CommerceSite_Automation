174723b10729c9ca175f1c6057575af9c21b156f
Akshay Jagadal <akshay.rajendra26@gmail.com>
2026-04-12 12:56:40 +0530

    Modified log4j file

--- src/test/java/com/ui/listeners/TestListener.java
+++ src/test/java/com/ui/listeners/TestListener.java
@@ -3,6 +3,7 @@ package com.ui.listeners;
 import java.util.Arrays;
 
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -11,16 +12,13 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.ui.tests.BaseTest;
 import com.utility.ExtentReporterUtility;
 import com.utility.LoggerUtility;
 
 public class TestListener implements ITestListener {
 	Logger logger = LoggerUtility.getLogger(this.getClass());
 	
-	
-	ExtentSparkReporter extentSparkReporter;
-	ExtentReports extentReports;
-	ExtentTest extentTest;
 
 	public void onTestStart(ITestResult result) {
 		logger.info(result.getMethod().getMethodName());
@@ -41,7 +39,10 @@ public class TestListener implements ITestListener {
 		logger.info(result.getMethod().getMethodName()+" "+"FAILED");
 		logger.error(result.getThrowable().toString());
 		ExtentReporterUtility.getTest().log(Status.FAIL, result.getMethod().getMethodName()+" "+"FAILED");
-
+		ExtentReporterUtility.getTest().log(Status.FAIL, result.getThrowable().toString());
+		
+		Object testClass = result.getInstance();
+		
 	}
 
 	public void onTestSkipped(ITestResult result) {
